cfafe8aa547869e1cf4b7fe54dbf21c3550a91e8
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-22 17:07:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = [1,2,3,4, "arrays!"]
-  array.pop("arrays!")
+  array_remove= array.pop
 end
 
 def retrieve_element_from_index(array, index_number)
