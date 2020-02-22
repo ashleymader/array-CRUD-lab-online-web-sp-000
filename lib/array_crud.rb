20414b9e647249cf394db073342b3d22c683cd12
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-22 17:08:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = [1,2,3,4, "arrays!"]
+  array = ["arrays!", 1,2,3,4]
   array_remove= array.pop
 end
 
